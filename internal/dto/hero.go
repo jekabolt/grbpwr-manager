ba1f752af8b88bcca915ee1400815209b49f9a4d
jekabolt <jekabolt@yahoo.com>
2023-08-13 19:27:50 +0300

    hero update

--- internal/dto/hero.go
+++ internal/dto/hero.go
@@ -4,6 +4,11 @@ import "time"
 
 type Hero struct {
 	TimeChanged time.Time
+	HeroLeft    HeroElement
+	HeroRight   HeroElement
+}
+
+type HeroElement struct {
 	ContentLink string
 	ContentType string
 	ExploreLink string
